a91056c3d240d331659194a925d95749d92e2a36
Mlkz <maho.mamulashvili@gmail.com>
2024-02-28 11:12:32 -0500

    Unique Paths from blind 75

--- matrices/index.js
+++ matrices/index.js
@@ -87,5 +87,21 @@ function rob(nums) {
 console.log(rob([1, 2, 3, 1])); // Output: 4
 console.log(rob([2, 7, 9, 3, 1])); // Output: 12
 
+function uniquePaths(m, n) {
+  const dp = Array.from({ length: m }, () => Array(n).fill(1));
+
+  for (let i = 1; i < m; i++) {
+    for (let j = 1; j < n; j++) {
+      dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+    }
+  }
+
+  return dp[m - 1][n - 1];
+}
+
+// Example usage:
+console.log(uniquePaths(3, 7)); // Output: 28
+console.log(uniquePaths(3, 2)); // Output: 3
+
 
 
