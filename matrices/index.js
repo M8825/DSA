715245db9cd72536cfb46bd5653847d387cbc22f
Mlkz <maho.mamulashvili@gmail.com>
2024-03-01 10:27:14 -0500

    Cycle problem

--- matrices/index.js
+++ matrices/index.js
@@ -140,4 +140,46 @@ console.log(merge([[1,3],[2,6],[8,10],[15,18]])); // Output: [[1,6],[8,10],[15,1
 console.log(merge([[1,4],[4,5]])); // Output: [[1,5]]
 
 
+function ListNode(val) {
+  this.val = val;
+  this.next = null;
+}
+
+function hasCycle(head) {
+  let slow = head;
+  let fast = head;
+
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+
+    if (slow === fast) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+// Example usage:
+let head = new ListNode(3);
+let node1 = new ListNode(2);
+let node2 = new ListNode(0);
+let node3 = new ListNode(-4);
+
+head.next = node1;
+node1.next = node2;
+node2.next = node3;
+node3.next = node1; // Creates a cycle
+
+console.log(hasCycle(head)); // Output: true
+
+let head2 = new ListNode(1);
+let node4 = new ListNode(2);
+
+head2.next = node4;
+node4.next = null; // No cycle
+
+console.log(hasCycle(head2)); // Output: false
+
 
