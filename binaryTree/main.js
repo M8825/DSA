d10018b67e8714532b0fa6dac8790b7809015cc5
Mlkz <maho.mamulashvili@gmail.com>
2024-03-18 19:13:41 -0400

    Binary tree value sum

--- binaryTree/main.js
+++ binaryTree/main.js
@@ -49,3 +49,49 @@ c.right = f;
 
 console.log(depthFirstValues(a));
 //    -> ['a', 'b', 'd', 'e', 'c', 'f']
+
+const treeSum = (root) => {
+  if (root === null) return 0;
+  let count = 0;
+  let stack = [root];
+
+  while (stack.length > 0) {
+    let nextNode = stack.pop();
+    count += nextNode.val;
+
+    if (nextNode.left) {
+      stack.push(nextNode.left);
+    }
+    if (nextNode.right) {
+      stack.push(nextNode.right);
+    }
+  }
+
+  return count;
+};
+const a = new Node(1);
+const b = new Node(6);
+const c = new Node(0);
+const d = new Node(3);
+const e = new Node(-6);
+const f = new Node(2);
+const g = new Node(2);
+const h = new Node(2);
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+e.left = g;
+f.right = h;
+
+//      1
+//    /   \
+//   6     0
+//  / \     \
+// 3   -6    2
+//    /       \
+//   2         2
+
+treeSum(a); // -> 10
