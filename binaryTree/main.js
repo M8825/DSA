14ba21082f25b8558150107dde2f980c9db49eca
Mlkz <maho.mamulashvili@gmail.com>
2024-03-18 19:18:06 -0400

    Binary tree includes

--- binaryTree/main.js
+++ binaryTree/main.js
@@ -95,3 +95,31 @@ f.right = h;
 //   2         2
 
 treeSum(a); // -> 10
+
+
+const a = new Node(1);
+const b = new Node(6);
+const c = new Node(0);
+const d = new Node(3);
+const e = new Node(-6);
+const f = new Node(2);
+const g = new Node(2);
+const h = new Node(2);
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+e.left = g;
+f.right = h;
+
+//      1
+//    /   \
+//   6     0
+//  / \     \
+// 3   -6    2
+//    /       \
+//   2         2
+
+treeSum(a); // -> 10
