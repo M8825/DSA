b742b47cafca93088059717c44ba28d6d55b9828
Mlkz <maho.mamulashvili@gmail.com>
2024-03-19 22:13:31 -0400

    Bottom right value

--- binaryTree/pathFinder.js
+++ binaryTree/pathFinder.js
@@ -1,70 +1,110 @@
-const pathFinder = (root, target) => {
-  if (root === null) return null;
-  if (root.val === target) return [ root.val ];
+// const pathFinder = (root, target) => {
+//   if (root === null) return null;
+//   if (root.val === target) return [ root.val ];
 
 
-  const leftPath = pathFinder(root.left, target);
-  if (leftPath !== null) return [ root.val, ...leftPath ];
+//   const leftPath = pathFinder(root.left, target);
+//   if (leftPath !== null) return [ root.val, ...leftPath ];
 
 
-  const rightPath = pathFinder(root.right, target);
-  if (rightPath !== null) return [ root.val, ...rightPath ];
+//   const rightPath = pathFinder(root.right, target);
+//   if (rightPath !== null) return [ root.val, ...rightPath ];
 
-  return null;
-};
+//   return null;
+// };
 
-const a = new Node("a");
-const b = new Node("b");
-const c = new Node("c");
-const d = new Node("d");
-const e = new Node("e");
-const f = new Node("f");
+// const a = new Node("a");
+// const b = new Node("b");
+// const c = new Node("c");
+// const d = new Node("d");
+// const e = new Node("e");
+// const f = new Node("f");
 
-a.left = b;
-a.right = c;
-b.left = d;
-b.right = e;
-c.right = f;
+// a.left = b;
+// a.right = c;
+// b.left = d;
+// b.right = e;
+// c.right = f;
 
-//      a
-//    /   \
-//   b     c
-//  / \     \
-// d   e     f
+// //      a
+// //    /   \
+// //   b     c
+// //  / \     \
+// // d   e     f
 
-pathFinder(a, 'e'); // -> [ 'a', 'b', 'e' ]
+// pathFinder(a, 'e'); // -> [ 'a', 'b', 'e' ]
 
-const treeValueCount = (root, target) => {
-  if (root === null) return 0;
+// const treeValueCount = (root, target) => {
+//   if (root === null) return 0;
 
+//   let queue = [root];
+//   let count = 0;
+
+//   while (queue.length > 0) {
+//     let nextNode = queue.shift();
+
+//     if (nextNode.val === target) {
+//       count++;
+//     }
+
+//     if (nextNode.right !== null) {
+//       queue.push(nextNode.right);
+//     }
+
+//     if (nextNode.left !== null) {
+//       queue.push(nextNode.left);
+//     }
+//   }
+
+//   return count;
+// };
+
+// const a = new Node(12);
+// const b = new Node(6);
+// const c = new Node(6);
+// const d = new Node(4);
+// const e = new Node(6);
+// const f = new Node(12);
+
+// a.left = b;
+// a.right = c;
+// b.left = d;
+// b.right = e;
+// c.right = f;
+
+// //      12
+// //    /   \
+// //   6     6
+// //  / \     \
+// // 4   6     12
+
+// treeValueCount(a,  6); // -> 3
+
+const bottomRightValue = (root) => {
   let queue = [root];
-  let count = 0;
+  let current = null;
 
   while (queue.length > 0) {
-    let nextNode = queue.shift();
+    current = queue.shift();
 
-    if (nextNode.val === target) {
-      count++;
+    if (current.left !== null) {
+      queue.push(current.left );
     }
 
-    if (nextNode.right !== null) {
-      queue.push(nextNode.right);
-    }
-
-    if (nextNode.left !== null) {
-      queue.push(nextNode.left);
+    if (current.right !== null) {
+      queue.push(current.right);
     }
   }
 
-  return count;
+  return current.val;
 };
 
-const a = new Node(12);
-const b = new Node(6);
-const c = new Node(6);
+const a = new Node(3);
+const b = new Node(11);
+const c = new Node(10);
 const d = new Node(4);
-const e = new Node(6);
-const f = new Node(12);
+const e = new Node(-2);
+const f = new Node(1);
 
 a.left = b;
 a.right = c;
@@ -72,10 +112,10 @@ b.left = d;
 b.right = e;
 c.right = f;
 
-//      12
-//    /   \
-//   6     6
-//  / \     \
-// 4   6     12
+//       3
+//    /    \
+//   11     10
+//  / \      \
+// 4   -2     1
 
-treeValueCount(a,  6); // -> 3
+bottomRightValue(a); // -> 1
