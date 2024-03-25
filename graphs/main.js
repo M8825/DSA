526eacf30e8b1feac1f9cb64ac743c05caa5d966
Mlkz <maho.mamulashvili@gmail.com>
2024-03-25 13:59:23 -0400

    Explore islands on grid

--- graphs/main.js
+++ graphs/main.js
@@ -1,23 +1,70 @@
-function hasPath(graph, src, dst) {
-  if (src === dst) return false;
+// function hasPath(graph, src, dst) {
+//   if (src === dst) return false;
 
-  for (let neighbor of graph[src]) {
-    if (hasPath(graph, neighbor, dst) === true) {
-      return true;
+//   for (let neighbor of graph[src]) {
+//     if (hasPath(graph, neighbor, dst) === true) {
+//       return true;
+//     }
+//   }
+
+//   return false;
+// }
+
+// const graph = {
+//   f: ['g', 'i'],
+//   g: ['h'],
+//   h: [],
+//   i: ['g', 'k'],
+//   j: ['i'],
+//   k: []
+// };
+
+// console.log(hasPath(graph, 'f', 'k')); // true
+
+const islandCount = (grid) => {
+  const visited = new Set();
+  let count = 0;
+
+  for (let r = 0; r < grid.length; r++) {
+    for (let c = 0; c < grid[0].length; c++) {
+      if (explore(grid, r, c, visited) === true) {
+        count++;
+      }
     }
   }
-
-  return false;
-}
-
-const graph = {
-  f: ['g', 'i'],
-  g: ['h'],
-  h: [],
-  i: ['g', 'k'],
-  j: ['i'],
-  k: []
+  return count;
 };
 
-console.log(hasPath(graph, 'f', 'k')); // true
+function explore(grid, r, c, visited) {
+  const rowInbound = 0 <= r && r < grid.length;
+  const colInbound = 0 <= c && c < grid[0].length;
+  if (!rowInbound || !colInbound) return false;
 
+  if (grid[r][c] === 'W') return false;
+
+  const pos = r + ',' + c;
+  if (visited.has(pos)) return false;
+  visited.add(pos);
+
+  explore(grid, r - 1, c, visited);
+  explore(grid, r + 1, c, visited);
+  explore(grid, r, c - 1, visited);
+  explore(grid, r, c + 1, visited);
+
+  return true;
+}
+
+module.exports = {
+  islandCount,
+};
+
+const grid = [
+  ['W', 'L', 'W', 'W', 'W'],
+  ['W', 'L', 'W', 'W', 'W'],
+  ['W', 'W', 'W', 'L', 'W'],
+  ['W', 'W', 'L', 'L', 'W'],
+  ['L', 'W', 'W', 'L', 'L'],
+  ['L', 'L', 'W', 'W', 'W'],
+];
+
+console.log(islandCount(grid)); // -> 3
