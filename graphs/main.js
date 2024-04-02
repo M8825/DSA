ae8979c6e0d02a2da7afabb8b774efe436d09905
Mlkz <maho.mamulashvili@gmail.com>
2024-04-02 15:58:46 -0400

    maxAreaOfIsland problem

--- graphs/main.js
+++ graphs/main.js
@@ -474,3 +474,33 @@ function dfs(board,i,j){
     dfs(board,i,j-1)
     return
 }
+
+class Solution {
+  dfs(grid, i, j) {
+      const m = grid.length;
+      const n = grid[0].length;
+
+      if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] === 0)
+          return 0;
+      grid[i][j] = 0;
+      return 1 + this.dfs(grid, i + 1, j) + this.dfs(grid, i, j - 1) + this.dfs(grid, i - 1, j) + this.dfs(grid, i, j + 1);
+  }
+
+  maxAreaOfIsland(grid) {
+      let res = 0;
+      const m = grid.length;
+      const n = grid[0].length;
+
+      for (let i = 0; i < m; i++) {
+          for (let j = 0; j < n; j++) {
+              if (grid[i][j] === 1) {
+                  res = Math.max(res, this.dfs(grid, i, j));
+              }
+          }
+      }
+      return res;
+  }
+}
+
+const solution = new Solution();
+console.log(solution.maxAreaOfIsland([[1,1,0,0,0],[1,1,0,0,0],[0,0,0,1,1],[0,0,0,1,1]]));
