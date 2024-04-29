6dbbfbcbb2e431ff12bfd8f42ca7581b20b4ea4e
Mlkz <55971701+M8825@users.noreply.github.com>
2024-04-29 10:17:33 +0000

    Generate Parenthesis

--- foo.js
+++ foo.js
@@ -21,3 +21,23 @@ var swapPairs = function(head) {
 
     return dummy.next;
 };
+
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function(n) {
+    let res = [];
+    backtrack('', 0, 0)
+    return res;
+
+    function backtrack(current, open, close) {
+        if (current.length === 2 * n) {
+            res.push(current);
+            return;
+        }
+
+        if (open < n) backtrack(current + '(', open + 1, close);
+        if (close < open) backtrack(current + ')', open, close + 1);
+    }
+};
