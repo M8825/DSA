e0ff7959698fcdc8081a45570af13c1603c0d05e
Mlkz <55971701+M8825@users.noreply.github.com>
2024-04-29 17:33:48 +0000

    Is valid parenthesis

--- foo.js
+++ foo.js
@@ -62,3 +62,27 @@ var mergeTwoLists = function(list1, list2) {
 
     return dummy.next;
 };
+
+
+function isValid(s) {
+    let stack = [];
+    let map = {
+        ')': '(',
+        ']': '[',
+        '}': '{'
+    }
+
+    for (let char of s) {
+        if (char in map) {
+            let topElement = stack.length === 0 ? '#' : stack.pop();
+            if (topElement != map[char]) {
+                return false;
+            }
+        } else {
+            stack.push(char);
+        }
+    }
+
+    return stack.length === 0;
+}
+
