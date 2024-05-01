3e480ab5a27db4aed39ad3164361ec0717d590cf
Mlkz <maho.mamulashvili@gmail.com>
2024-05-01 15:37:13 -0400

    lastStoneWeight problem

--- graphs/main.js
+++ graphs/main.js
@@ -388,3 +388,16 @@ var kClosest = function(points, k) {
         return point[0] * point[0] + point[1] * point[1]
     }
 }
+
+/**
+ * @param {number[]} stones
+ * @return {number}
+ */
+var lastStoneWeight = function(stones) {
+  while(stones.length>1){
+      stones.sort((a,b)=>b-a); //sort the remaining stones in decending order;
+      stones[1]=stones[0]-stones[1]; //smash the first and second stones ie the stones with largest weight ans assign the remaining stone weight to 1st index
+      stones.shift();//shift the array to get rid of the 0 index
+  }
+  return stones[0] //return the 0 index value ie the resultl
+};
