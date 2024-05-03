9d531c3f02c44a04ece721234bc627c1fc3a8601
Mlkz <55971701+M8825@users.noreply.github.com>
2024-05-03 14:18:49 +0000

    Is palindrome

--- foo.js
+++ foo.js
@@ -160,3 +160,19 @@ var fourSum = function(nums, target) {
         }
     }
 };
+
+var isPalindrome = function(x) {
+  const str = x.toString();
+  let left = 0;
+  let right = str.length - 1;
+
+  while (left < right) {
+    if (str.charAt(left) !== str.charAt(right)) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+
+  return true;
+};
