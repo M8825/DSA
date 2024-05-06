834b12e8b69c630dc006940e4941735afe5a8b1b
Mlkz <55971701+M8825@users.noreply.github.com>
2024-05-06 21:59:40 +0000

    Is Valid Sudoku

--- blind150.js
+++ blind150.js
@@ -23,3 +23,30 @@ var productExceptSelf = function(nums) {
 
   return res;
 };
+
+
+
+function isValidSudoku(board) {
+    const rows = Array(9).fill(0).map(() => new Set());
+    const cols = Array(9).fill(0).map(() => new Set());
+    const squares = Array(9).fill(0).map(() => new Set());
+
+    for (let r = 0; r < board.length; r++) {
+        for (let c = 0; c < board[0].length; c++) {
+            const num = board[r][c];
+            if (num === ".") continue;
+
+            const boxIdx = Math.floor(r / 3) * 3 + Math.floor(c / 3);
+            if (rows[r].has(num) || cols[c].has(num) || squares[boxIdx].has(num)) {
+                return false;
+            }
+
+            rows[r].add(num);
+            cols[c].add(num);
+            squares[boxIdx].add(num);
+
+        }
+    }
+
+    return true;
+}
