48217d9f108205bfe741e375feee222a93fbcf11
Mlkz <55971701+M8825@users.noreply.github.com>
2024-05-14 15:31:07 +0000

    Check sudoku for duplicates

---
+++ Array and string/is_valid_sudoky.py
@@ -0,0 +1,35 @@
+from typing import List
+
+from test_framework import generic_test
+import collections
+
+
+# Check if a partially filled matrix has any conflicts.
+def is_valid_sudoku(partial_assignment: List[List[int]]) -> bool:
+    n = len(partial_assignment)
+    rows = collections.defaultdict(set)
+    cols = collections.defaultdict(set)
+    squares = collections.defaultdict(set)
+
+    for r in range(n):
+        for c in range(n):
+            num = partial_assignment[r][c]
+            if num == 0:
+                continue
+
+            if (num in rows[r]
+                or num in cols[c]
+                or num in squares[(r // 3, c // 3)]):
+                return False
+
+            rows[r].add(num)
+            cols[c].add(num)
+            squares[((r // 3, c // 3))].add(num)
+
+    return True
+
+
+if __name__ == '__main__':
+    exit(
+        generic_test.generic_test_main('is_valid_sudoku.py',
+                                       'is_valid_sudoku.tsv', is_valid_sudoku))
