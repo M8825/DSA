821fe856a640850953b6d88c2b4ba818a63a9079
Mlkz <55971701+M8825@users.noreply.github.com>
2024-05-16 13:05:23 +0000

    Can reach goal

---
+++ Array and string/advance_by_offset.py
@@ -0,0 +1,18 @@
+from typing import List
+
+from test_framework import generic_test
+
+def can_reach_end(A: List[int]) -> bool:
+    goal = len(A) - 1
+
+    for i in reversed(range(len(A) - 1)):
+        if A[i] + i >= goal:
+            goal = i
+
+    return goal == 0
+
+if __name__ == '__main__':
+    exit(
+        generic_test.generic_test_main('advance_by_offsets.py',
+                                       'advance_by_offsets.tsv',
+                                       can_reach_end))
