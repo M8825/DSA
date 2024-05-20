98e75d0de03a3ef53a5d21ba451260076c249603
Mlkz <55971701+M8825@users.noreply.github.com>
2024-05-20 10:16:34 +0000

    Add replace and remove algorithm

---
+++ Array and string/replace_and_remove.py
@@ -0,0 +1,28 @@
+from typing import List
+
+def replace_and_remove(size: int, s: List[str]) -> int:
+    write_idx, a_count = 0, 0
+    for i in range(size):
+        if s[i] != 'b':
+            s[write_idx] = s[i]
+            write_idx += 1
+        if s[i] == 'a':
+            a_count += 1
+
+    cur_idx = write_idx - 1
+    write_idx += a_count - 1
+    final_size = write_idx + 1
+    while cur_idx >= 0:
+        if s[cur_idx] == 'a':
+            s[write_idx - 1:write_idx + 1] = 'dd'
+            write_idx -= 2
+        else:
+            s[write_idx] = s[cur_idx]
+            write_idx -= 1
+        cur_idx -= 1
+
+    print(s)
+    return final_size
+
+
+replace_and_remove(4, ['a', 'c', 'b', 'a', '', '', ''])
