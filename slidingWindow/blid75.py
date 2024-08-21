d87b58d3c12e1befb1343bdf05e08c5d2b876443
Mlkz <55971701+M8825@users.noreply.github.com>
2024-08-21 11:34:27 +0000

    compress

--- slidingWindow/blid75.py
+++ slidingWindow/blid75.py
@@ -28,3 +28,25 @@ def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
                 count += 1
 
     return count >= n
+
+
+class Solution:
+    def compress(self, chars: List[str]) -> int: # ["a","a","b","b","c","c","c"]
+        write = 0
+        count = 1
+
+        for read in range(1, len(chars) + 1):
+            if read < len(chars) and chars[read] == chars[read - 1]:
+                count += 1
+            else:
+                chars[write] = chars[read - 1]
+                write += 1
+
+                if count > 1:
+                    for digit in str(count):
+                        chars[write] = digit
+                        write += 1
+                count = 1
+
+        return write
+
