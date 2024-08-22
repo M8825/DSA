7d0e5178873c1462669205560148f98b96d8b897
Mlkz <55971701+M8825@users.noreply.github.com>
2024-08-22 09:16:56 +0000

    max vowels

--- slidingWindow/blid75.py
+++ slidingWindow/blid75.py
@@ -67,3 +67,27 @@ class Solution:
                 right -= 1
 
         return max_area
+
+
+class Solution:
+    def maxVowels(self, s: str, k: int) -> int:
+        vowels = set('aeiou')
+        max_count = 0
+        current_count = 0
+
+        # initialize window
+        for i in range(k):
+            if s[i] in vowels:
+                current_count += 1
+        max_count = current_count
+
+        for i in range(k, len(s)):
+            if s[i] in vowels:
+                current_count += 1
+            if s[i - k] in vowels:
+                current_count -= 1
+            max_count = max(max_count, current_count)
+
+        return max_count
+
+
