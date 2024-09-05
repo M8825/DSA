ec9f3ce34a96ad713f55763b10e4b109139807cc
Mlkz <55971701+M8825@users.noreply.github.com>
2024-09-05 10:12:55 +0000

    Add asteroid colision

--- slidingWindow/blid75.py
+++ slidingWindow/blid75.py
@@ -191,3 +191,25 @@ class Solution(object):
         a.sort()
         b.sort()
         return a==b
+
+
+class Solution(object):
+    def asteroidCollision(self, asteroids):
+        """
+        :type asteroids: List[int]
+        :rtype: List[int]
+        """
+        stack = []
+
+        for asteroid in asteroids:
+            while stack and asteroid < 0 < stack[-1]:
+                if stack[-1] < -asteroid:
+                    stack.pop()
+                    continue
+                elif stack[-1] == -asteroid:
+                    stack.pop()
+                break
+            else:
+                stack.append(asteroid)
+
+        return stack
