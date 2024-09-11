78abe8f54f2c6ac262f60ca0824ddafc5091faef
Mlkz <55971701+M8825@users.noreply.github.com>
2024-09-11 10:15:24 +0000

    Add coin change

--- slidingWindow/mediums.js
+++ slidingWindow/mediums.js
@@ -54,5 +54,25 @@ function longestWord(sentence) {
 
 // Example usage:
 const sentence = 'The quick brown fox jumps over the lazy dog';
-const output = longestWord(sentence);
-console.log(output); // Output: "jumps"
+const output2 = longestWord(sentence);
+console.log(output2); // Output: "jumps"
+
+
+function coinChange(coins, amount) {
+  const dp = Array(amount + 1).fill(Infinity);
+  dp[0] = 0;
+
+  for (let i = 1; i <= amount; i++) {
+    for (const coin of coins) {
+      if (i - coin >= 0) {
+        dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+      }
+    }
+  }
+
+  return dp[amount] === Infinity ? -1 : dp[amount];
+}
+
+// Example usage:
+console.log(coinChange([1, 2, 5], 11));  // Output: 3
+console.log(coinChange([2], 3));         // Output:​⬤
