5fd599348f01d013e7e702c907d9de6156df54c8
Mlkz <55971701+M8825@users.noreply.github.com>
2024-10-07 14:16:27 +0000

    Add length of longest Substring

--- slidingWindow/bar.py
+++ slidingWindow/bar.py
@@ -9,3 +9,16 @@ def twoSum(numbers: List[int], target: int) -> List[int]:
         else:
             right -= 1
     return []
+
+
+def lengthOfLongestSubstring(s: str) -> int:
+    char_map = {}
+    left, max_len = 0, 0
+
+    for right in range(len(s)):
+        if s[right] in char_map and char_map[s[right]] >= left:
+            left = char_map[s[right]] + 1
+        char_map[s[right]] = right
+        max_len = max(max_len, right - left + 1)
+
+    return max_len
