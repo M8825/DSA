b149c0d2916c2ae6a82e85726e5da432008fea7a
Mlkz <55971701+M8825@users.noreply.github.com>
2024-10-09 10:01:22 +0000

    Add max area length

--- slidingWindow/bar.py
+++ slidingWindow/bar.py
@@ -47,3 +47,19 @@ def threeSum(nums: List[int]) -> List[List[int]]:
                 right -= 1
 
     return res
+
+
+def maxArea(height: List[int]) -> int:
+    left, right = 0, len(height) - 1
+    max_water = 0
+
+    while left < right:
+        h = min(height[left], height[right])
+        max_water = max(max_water, h * (right - left))
+
+        if height[left] < height[right]:
+            left += 1
+        else:
+            right -= 1
+
+    return max_water
