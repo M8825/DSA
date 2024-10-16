4d12f24ad0f49bf7c7337c98fbc5b44aac067033
Mlkz <55971701+M8825@users.noreply.github.com>
2024-10-16 12:11:09 +0000

    Add rotate list

--- slidingWindow/baz.py
+++ slidingWindow/baz.py
@@ -65,3 +65,13 @@ def findPeakElement(nums: List[int]) -> int:
         else:
             left = mid + 1
     return left
+
+def rotate(matrix: List[List[int]]) -> None:
+    n = len(matrix)
+    # Transpose the matrix
+    for i in range(n):
+        for j in range(i, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    # Reverse each row
+    for i in range(n):
+        matrix[i].reverse()
