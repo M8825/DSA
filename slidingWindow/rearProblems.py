0da98963d967b3da42d84ead6ebecaaf6a8869d4
Mlkz <55971701+M8825@users.noreply.github.com>
2024-10-22 15:32:14 +0000

    add max area of islands

--- slidingWindow/rearProblems.py
+++ slidingWindow/rearProblems.py
@@ -32,3 +32,18 @@ def numMatchingSubseq(s, words):
         return True
 
     return sum(is_subsequence(word) for word in words)
+
+
+def maxAreaOfIsland(grid):
+    def dfs(x, y):
+        if x < 0 or y < 0 or x >= len(grid) or y >= len(grid[0]) or grid[x][y] == 0:
+            return 0
+        grid[x][y] = 0
+        return 1 + dfs(x + 1, y) + dfs(x - 1, y) + dfs(x, y + 1) + dfs(x, y - 1)
+
+    max_area = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == 1:
+                max_area = max(max_area, dfs(i, j))
+    return max_area
