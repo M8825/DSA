63e4bfdc986b16087f58feec9e8984c2376a0414
Mlkz <55971701+M8825@users.noreply.github.com>
2024-10-23 10:21:43 +0000

    add build tree

--- slidingWindow/rearProblems.py
+++ slidingWindow/rearProblems.py
@@ -69,3 +69,22 @@ def isPossible(nums):
             return False
         freq[num] -= 1
     return True
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def buildTree(preorder, inorder):
+    if not preorder or not inorder:
+        return None
+
+    root_val = preorder.pop(0)
+    root = TreeNode(root_val)
+    inorder_index = inorder.index(root_val)
+
+    root.left = buildTree(preorder, inorder[:inorder_index])
+    root.right = buildTree(preorder, inorder[inorder_index + 1:])
+
+    return root
