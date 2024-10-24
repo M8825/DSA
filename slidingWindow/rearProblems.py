294e986c109b9bfbb1fe6999568b6d2aa5598705
Mlkz <55971701+M8825@users.noreply.github.com>
2024-10-24 13:34:23 +0000

    add least bricks

--- slidingWindow/rearProblems.py
+++ slidingWindow/rearProblems.py
@@ -88,3 +88,16 @@ def buildTree(preorder, inorder):
     root.right = buildTree(preorder, inorder[inorder_index + 1:])
 
     return root
+
+
+def leastBricks(wall):
+    edge_count = defaultdict(int)
+
+    for row in wall:
+        edge_position = 0
+        for brick in row[:-1]:  # skip the last brick to avoid counting the edge
+            edge_position += brick
+            edge_count[edge_position] += 1
+
+    # Minimum bricks cut will be total rows minus the maximum number of aligned edges
+    return len(wall) - max(edge_count.values(), default=0)
