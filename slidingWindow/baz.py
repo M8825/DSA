3d8dc234740052d4ca9aea730f9bd335fcbdbd54
Mlkz <55971701+M8825@users.noreply.github.com>
2024-10-29 17:12:25 +0000

    Add min domino rotations

--- slidingWindow/baz.py
+++ slidingWindow/baz.py
@@ -152,3 +152,21 @@ def findAndReplacePattern(words, pattern):
         return True
 
     return [word for word in words if matches(word)]
+
+
+def minDominoRotations(A, B):
+    def check(x):
+        rotations_a = rotations_b = 0
+        for i in range(len(A)):
+            if A[i] != x and B[i] != x:
+                return -1
+            elif A[i] != x:
+                rotations_a += 1
+            elif B[i] != x:
+                rotations_b += 1
+        return min(rotations_a, rotations_b)
+
+    rotations = check(A[0])
+    if rotations != -1 or A[0] == B[0]:
+        return rotations
+    return check(B[0])
