62fe252f47fa4587019bdf0b93aa4ab877ed4e43
Mlkz <55971701+M8825@users.noreply.github.com>
2024-10-30 12:56:47 +0000

    Add sum even grandparent

--- slidingWindow/baz.py
+++ slidingWindow/baz.py
@@ -198,3 +198,23 @@ def partitionLabels(s):
             anchor = i + 1
 
     return result
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def sumEvenGrandparent(root):
+    def dfs(node, parent, grandparent):
+        if not node:
+            return 0
+        sum_val = 0
+        if grandparent and grandparent.val % 2 == 0:
+            sum_val += node.val
+        sum_val += dfs(node.left, node, parent)
+        sum_val += dfs(node.right, node, parent)
+        return sum_val
+
+    return dfs(root, None, None)
