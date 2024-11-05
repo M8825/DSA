9e876ef8da26b918bab7fa754753d1bce80da52f
Mlkz <55971701+M8825@users.noreply.github.com>
2024-11-05 19:13:48 +0000

    Add set zero

--- slidingWindow/baz.py
+++ slidingWindow/baz.py
@@ -260,3 +260,30 @@ def threeSum(nums):
                 right -= 1
 
     return result
+
+
+def setZeroes(matrix):
+    rows, cols = len(matrix), len(matrix[0])
+    row_zero = False
+
+    for i in range(rows):
+        for j in range(cols):
+            if matrix[i][j] == 0:
+                matrix[0][j] = 0
+                if i > 0:
+                    matrix[i][0] = 0
+                else:
+                    row_zero = True
+
+    for i in range(1, rows):
+        for j in range(1, cols):
+            if matrix[i][0] == 0 or matrix[0][j] == 0:
+                matrix[i][j] = 0
+
+    if matrix[0][0] == 0:
+        for i in range(rows):
+            matrix[i][0] = 0
+
+    if row_zero:
+        for j in range(cols):
+            matrix[0][j] = 0
