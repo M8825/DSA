5ca0933d31013707aaf92d1eb6b82557e64da2ed
Mlkz <55971701+M8825@users.noreply.github.com>
2024-11-06 10:13:45 +0000

    Add unique paths

--- slidingWindow/baz.py
+++ slidingWindow/baz.py
@@ -287,3 +287,12 @@ def setZeroes(matrix):
     if row_zero:
         for j in range(cols):
             matrix[0][j] = 0
+
+def uniquePaths(m, n):
+    dp = [[1] * n for _ in range(m)]
+
+    for i in range(1, m):
+        for j in range(1, n):
+            dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+
+    return dp[-1][-1]
