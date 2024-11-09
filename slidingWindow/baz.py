404ec4db5f896d327b2e17add2883df4c0d1f872
Mlkz <55971701+M8825@users.noreply.github.com>
2024-11-09 02:41:12 +0000

    add max area

--- slidingWindow/baz.py
+++ slidingWindow/baz.py
@@ -218,3 +218,19 @@ def sumEvenGrandparent(root):
         return sum_val
 
     return dfs(root, None, None)
+
+
+def maxArea(height):
+    left, right = 0, len(height) - 1
+    max_area = 0
+
+    while left < right:
+        width = right - left
+        max_area = max(max_area, min(height[left], height[right]) * width)
+
+        if height[left] < height[right]:
+            left += 1
+        else:
+            right -= 1
+
+    return max_area
