9b74f1f72e8d70734363e2acbeeae9c52eb1de43
Mlkz <55971701+M8825@users.noreply.github.com>
2024-11-22 15:32:14 +0000

    add num matching subseq

--- slidingWindow/rearProblems.py
+++ slidingWindow/rearProblems.py
@@ -11,3 +11,24 @@ def leastBricks(wall):
 
     # Minimum bricks cut will be total rows minus the maximum number of aligned edges
     return len(wall) - max(edge_count.values(), default=0)
+
+
+from bisect import bisect_right
+
+def numMatchingSubseq(s, words):
+    char_indices = defaultdict(list)
+    for idx, char in enumerate(s):
+        char_indices[char].append(idx)
+
+    def is_subsequence(word):
+        prev_index = -1
+        for char in word:
+            if char not in char_indices:
+                return False
+            pos = bisect_right(char_indices[char], prev_index)
+            if pos == len(char_indices[char]):
+                return False
+            prev_index = char_indices[char][pos]
+        return True
+
+    return sum(is_subsequence(word) for word in words)
