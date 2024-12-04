fca261b6f2f9ff039a4c5ed1c4816aebc4a54820
Mlkz <55971701+M8825@users.noreply.github.com>
2024-12-04 06:58:39 +0000

    Add longest palindrom

--- slidingWindow/baz.py
+++ slidingWindow/baz.py
@@ -453,3 +453,17 @@ def lengthOfLongestSubstring(s: str) -> int:
         char_map[s[right]] = right
         max_length = max(max_length, right - left + 1)
     return max_length
+
+def longestPalindrome(s: str) -> str:
+    def expand_around_center(left: int, right: int) -> str:
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            left -= 1
+            right += 1
+        return s[left + 1:right]
+
+    longest = ""
+    for i in range(len(s)):
+        odd_palindrome = expand_around_center(i, i)
+        even_palindrome = expand_around_center(i, i + 1)
+        longest = max(longest, odd_palindrome, even_palindrome, key=len)
+    return longest
