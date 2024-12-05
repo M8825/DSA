7b1104237d769946fa9da6c707cde6264a52d7bc
Mlkz <55971701+M8825@users.noreply.github.com>
2024-12-05 06:59:11 +0000

    Add max area

--- slidingWindow/baz.py
+++ slidingWindow/baz.py
@@ -467,3 +467,16 @@ def longestPalindrome(s: str) -> str:
         even_palindrome = expand_around_center(i, i + 1)
         longest = max(longest, odd_palindrome, even_palindrome, key=len)
     return longest
+
+
+def maxArea(height: List[int]) -> int:
+    left, right = 0, len(height) - 1
+    max_area = 0
+    while left < right:
+        width = right - left
+        max_area = max(max_area, min(height[left], height[right]) * width)
+        if height[left] < height[right]:
+            left += 1
+        else:
+            right -= 1
+    return max_area
