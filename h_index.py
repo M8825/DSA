44e2be8330d88de6f1ee96c0b68bc5feac0fa726
Mlkz <maho.mamulashvili@gmail.com>
2024-12-17 02:13:14 -0500

    add strange printer

--- h_index.py
+++ h_index.py
@@ -6,3 +6,18 @@ def h_index(citations: List[int]) -> int:
         if c >= n - i:
             return n - i
     return 0
+
+
+def strangePrinter(s: str) -> int:
+    n = len(s)
+    dp = [[0] * n for _ in range(n)]
+
+    for i in range(n - 1, -1, -1):
+        dp[i][i] = 1
+        for j in range(i + 1, n):
+            dp[i][j] = dp[i + 1][j] + 1
+            for k in range(i, j):
+                if s[k] == s[j]:
+                    dp[i][j] = min(dp[i][j], dp[i][k] + dp[k + 1][j - 1])
+
+    return dp[0][n - 1]
