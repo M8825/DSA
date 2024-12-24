91436a809a988d322e01b0b29cf1c8f417d1cb50
Mlkz <maho.mamulashvili@gmail.com>
2024-12-23 23:41:11 -0500

    add strange printer

--- h_index.py
+++ h_index.py
@@ -116,3 +116,21 @@ def minArea(image, x, y):
     bounds = [x, x, y, y]
     dfs(x, y, bounds)
     return (bounds[1] - bounds[0] + 1) * (bounds[3] - bounds[2] + 1)
+
+
+def strangePrinter(s):
+    n = len(s)
+    dp = [[float('inf')] * n for _ in range(n)]
+
+    for i in range(n):
+        dp[i][i] = 1
+
+    for length in range(2, n+1):
+        for i in range(n-length+1):
+            j = i + length - 1
+            for k in range(i, j):
+                dp[i][j] = min(dp[i][j], dp[i][k] + dp[k+1][j])
+            if s[i] == s[j]:
+                dp[i][j] -= 1
+
+    return dp[0][n-1]
