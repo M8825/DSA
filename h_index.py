e9904c5c1ab5cfe850afd04acde64434ad6b5e1b
Mlkz <maho.mamulashvili@gmail.com>
2025-01-14 22:55:52 +0300

    Add find peak element

--- h_index.py
+++ h_index.py
@@ -355,3 +355,16 @@ def maxArea(height: List[int]) -> int:
             right -= 1
 
     return max_area
+
+
+def findPeakElement(nums: List[int]) -> int:
+    left, right = 0, len(nums) - 1
+
+    while left < right:
+        mid = (left + right) // 2
+        if nums[mid] > nums[mid + 1]:
+            right = mid
+        else:
+            left = mid + 1
+
+    return left
