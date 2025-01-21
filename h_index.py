334e1d3a63d8defd5c632da08985ff6667dfb08a
Mlkz <maho.mamulashvili@gmail.com>
2025-01-21 14:48:00 +0400

    Add two sum

--- h_index.py
+++ h_index.py
@@ -376,3 +376,13 @@ def twoSum(nums, target):
         if complement in num_map:
             return [num_map[complement], i]
         num_map[num] = i
+
+def lengthOfLongestSubstring(s):
+    char_map = {}
+    left, max_length = 0, 0
+    for right, char in enumerate(s):
+        if char in char_map and char_map[char] >= left:
+            left = char_map[char] + 1
+        char_map[char] = right
+        max_length = max(max_length, right - left + 1)
+    return max_length
