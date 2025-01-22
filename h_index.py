258bf43da1600f45e68c36a0ea6bb47fed95e534
Mlkz <maho.mamulashvili@gmail.com>
2025-01-22 14:45:27 +0400

    Add two sum

--- h_index.py
+++ h_index.py
@@ -368,3 +368,11 @@ def findPeakElement(nums: List[int]) -> int:
             left = mid + 1
 
     return left
+
+def twoSum(nums, target):
+    num_map = {}
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in num_map:
+            return [num_map[complement], i]
+        num_map[num] = i
