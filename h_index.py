76fa5ec7d7d6ae14ecabe6bac87686f6d46e903e
Mlkz <maho.mamulashvili@gmail.com>
2025-01-22 14:49:05 +0400

    Add binary search

--- h_index.py
+++ h_index.py
@@ -403,3 +403,15 @@ def mergeTwoLists(l1, l2):
         current = current.next
     current.next = l1 or l2
     return dummy.next
+
+def binarySearch(nums, target):
+    left, right = 0, len(nums) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
