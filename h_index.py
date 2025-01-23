08305c2f4ac702050a4e3262ae87054b38aaec89
Mlkz <maho.mamulashvili@gmail.com>
2025-01-23 14:49:39 +0400

    Add max sub array

--- h_index.py
+++ h_index.py
@@ -415,3 +415,10 @@ def binarySearch(nums, target):
         else:
             right = mid - 1
     return -1
+
+def maxSubArray(nums):
+    max_sum = current_sum = nums[0]
+    for num in nums[1:]:
+        current_sum = max(num, current_sum + num)
+        max_sum = max(max_sum, current_sum)
+    return max_sum
